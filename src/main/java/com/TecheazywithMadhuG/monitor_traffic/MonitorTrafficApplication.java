4d81d0517918588c962502cc838d179e2cdd6893
Madhu Gundarapu <110929381+madhugundarapu@users.noreply.github.com>
2025-08-18 14:04:50 +0530

    Initial version

---
+++ src/main/java/com/TecheazywithMadhuG/monitor_traffic/MonitorTrafficApplication.java
@@ -0,0 +1,13 @@
+package com.TecheazywithMadhuG.monitor_traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorTrafficApplication {
+		//Trying Git
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorTrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TecheazywithMadhuG/monitor_traffic/MonitorTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TecheazywithMadhuG.monitor_traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
