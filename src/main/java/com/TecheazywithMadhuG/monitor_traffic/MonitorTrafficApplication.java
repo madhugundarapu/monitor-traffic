7d2d8d25b42159e8f553a09df842d7a815c967f5
Madhu Gundarapu <110929381+madhugundarapu@users.noreply.github.com>
2025-08-18 15:04:18 +0530

    enhanced coments

--- src/main/java/com/TecheazywithMadhuG/monitor_traffic/MonitorTrafficApplication.java
+++ src/main/java/com/TecheazywithMadhuG/monitor_traffic/MonitorTrafficApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MonitorTrafficApplication {
-		//Trying Git locally
+		//Trying Git locally with stage
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorTrafficApplication.class, args);
 	}
