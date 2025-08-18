44d39930507c742da70f4a5e24594fd9892001bc
Madhu Gundarapu <110929381+madhugundarapu@users.noreply.github.com>
2025-08-18 20:19:42 +0530

    added newbranch

--- src/main/java/com/TecheazywithMadhuG/monitor_traffic/MonitorTrafficApplication.java
+++ src/main/java/com/TecheazywithMadhuG/monitor_traffic/MonitorTrafficApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MonitorTrafficApplication {
 		//Trying Git locally - corrected, analytics branch
+		// changes for new branch
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorTrafficApplication.class, args);
 	}
